6cbfdc6ad5a5235ffbbfd61f46a27fca054ee4bb
ogomii <mi.ogo@wp.pl>
2022-09-18 21:06:51 +0200

    initial commit

---
+++ bigMutex.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <iostream>
+#include <pthread.h>
+
+#define NUM_OF_THREADS 2
+
+/*
+  mutex lock for adding to shared variable in for loop
+  elapsedTime averaged around 4.1 s
+*/
+pthread_mutex_t mutex;
+
+void *add(void *input) {
+  int *a = (int *)input;
+
+  pthread_mutex_lock(&mutex);
+
+  for (int i = 0; i < 1e9; i++) {
+    *a += 1;
+  }
+
+  // std::cout << "Result of thread (id: " << pthread_self() << " ) : " << *a
+  // << std::endl;
+
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+int main(void) {
+  pthread_t threads[NUM_OF_THREADS];
+  int result = 0;
+
+  std::cout << "Starting threads" << std::endl;
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  pthread_create(&threads[0], NULL, add, &result);
+  pthread_create(&threads[1], NULL, add, &result);
+
+  pthread_join(threads[0], NULL);
+  pthread_join(threads[1], NULL);
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsedTime = endTime - startTime;
+
+  std::cout << "Threads finished, result = " << result << std::endl;
+  std::cout << "in time: " << elapsedTime.count() << std::endl;
+}
